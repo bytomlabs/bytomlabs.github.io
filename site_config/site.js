e2f1f945b4bc98a425a2ece886649efec8df2471
黄兴隆 <1292028314@qq.com>
2019-01-21 16:26:30 +0800

    change file

--- site_config/site.js
+++ site_config/site.js
@@ -6,25 +6,11 @@ export default {
     defaultSearch: 'google', // 默认搜索引擎，baidu或者google
     defaultLanguage: 'en-us',
     'en-us': {
-        pageMenu: [{
-                key: 'home', // 用作顶部菜单的选中
-                text: 'HOME',
-                link: '/en-us/index.html',
-            },
+        pageMenu: [
             {
                 key: 'docs',
                 text: 'DOCS',
                 link: '/en-us/docs/demo1.html',
-            },
-            {
-                key: 'blog',
-                text: 'BLOG',
-                link: '/en-us/blog/index.html',
-            },
-            {
-                key: 'community',
-                text: 'COMMUNITY',
-                link: '/en-us/community/index.html',
             },
         ],
         disclaimer: {
@@ -62,25 +48,11 @@ export default {
         copyright: 'Copyright © 2018 Bytomxxxxxxxx',
     },
     'zh-cn': {
-        pageMenu: [{
-                key: 'home',
-                text: '首页',
-                link: '/zh-cn/index.html',
-            },
+        pageMenu: [
             {
                 key: 'docs',
                 text: '文档',
                 link: '/zh-cn/docs/mydoc_index.html',
-            },
-            {
-                key: 'blog',
-                text: '博客',
-                link: '/zh-cn/blog/index.html',
-            },
-            {
-                key: 'community',
-                text: '社区',
-                link: '/zh-cn/community/index.html',
             },
         ],
         disclaimer: {
