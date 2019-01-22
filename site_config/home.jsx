fcf6f7db7753ea9080e744960b14ede8f68c0b48
黄兴隆 <1292028314@qq.com>
2019-01-22 15:43:46 +0800

    delete css

--- site_config/home.jsx
+++ site_config/home.jsx
@@ -4,11 +4,11 @@ export default {
   'zh-cn': {
     brand: {
       brandName: 'Bytom开发者文档',
-      briefIntroduction: 'bytom介绍',
+      briefIntroduction: 'bytom文档',
       buttons: [
         {
           text: '立即开始',
-          link: '/zh-cn/docs/demo1.html',
+          link: '/zh-cn/docs/00_index.html',
           type: 'primary',
         },
         {
@@ -19,52 +19,27 @@ export default {
       ],
     },
     introduction: {
-      title: '比原链介绍',
-      desc: '比原链(Bytom)，比特世界和原子世界的链接者！',
-      img: '/img/architecture.png',
+      title: '',
+      desc: '',
+      img: '',
     },
     features: {
-      title: '特性一览',
+      title: '',
       list: [
         {
-          img: '/img/feature_transpart.png',
-          title: '特性1',
-          content: '专注资产流通领域的公有区块链平台',
-        },
-        {
-          img: '/img/feature_loadbalances.png',
-          title: '特性2',
-          content: '人工智能友好型的pow算法',
-        },
-        {
-          img: '/img/feature_service.png',
-          title: '特性3',
+          img: '',
+          title: '',
           content: '',
-        },
-        {
-          img: '/img/feature_hogh.png',
-          title: '特性4',
-          content: '特性4的简单概括',
-        },
-        {
-          img: '/img/feature_runtime.png',
-          title: '特性5',
-          content: '特性5的简单概括',
-        },
-        {
-          img: '/img/feature_maintenance.png',
-          title: '特性6',
-          content: '特性6的简单概括',
         },
       ],
     },
     start: {
-      title: '快速开始',
-      desc: '简单描述',
-      img: '/img/quick_start.png',
+      title: '',
+      desc: '',
+      img: '',
       button: {
-        text: '阅读更多',
-        link: '/zh-cn/docs/00_index.html',
+        text: '',
+        link: '',
       },
     },
     users: {
@@ -77,90 +52,50 @@ export default {
   },
   'en-us': {
     brand: {
-      brandName: 'brandName',
-      briefIntroduction: 'some description of product',
+      brandName: 'Bytom development document',
+      briefIntroduction: 'bytom document',
       buttons: [
         {
           text: 'Quick Start',
-          link: '/en-us/docs/demo1.html',
+          link: '/en-us/docs/00_index.html',
           type: 'primary',
         },
         {
-          text: 'View on Github',
-          link: '',
+          text: 'Edit Document',
+          link: 'https://github.com/Bytom/bytom',
           type: 'normal',
         },
       ],
     },
     introduction: {
-      title: 'introduction title',
-      desc: 'some introduction of your product',
-      img: '/img/architecture.png',
+      title: '',
+      desc: '',
+      img: '',
     },
     features: {
-      title: 'Feature List',
+      title: '',
       list: [
         {
-          img: '/img/feature_transpart.png',
-          title: 'feature1',
-          content: 'feature description',
+          img: '',
+          title: '',
+          content: '',
         },
-        {
-          img: '/img/feature_loadbalances.png',
-          title: 'feature2',
-          content: 'feature description',
-        },
-        {
-          img: '/img/feature_service.png',
-          title: 'feature3',
-          content: 'feature description',
-        },
-        {
-          img: '/img/feature_hogh.png',
-          title: 'feature4',
-          content: 'feature description',
-        },
-        {
-          img: '/img/feature_runtime.png',
-          title: 'feature5',
-          content: 'feature description',
-        },
-        {
-          img: '/img/feature_maintenance.png',
-          title: 'feature6',
-          content: 'feature description',
-        }
       ]
     },
     start: {
-      title: 'Quick start',
-      desc: 'some description text',
-      img: '/img/quick_start.png',
+      title: '',
+      desc: '',
+      img: '',
       button: {
-        text: 'READ MORE',
-        link: '/en-us/docs/demo1.html',
+        text: '',
+        link: '',
       },
     },
     users: {
-      title: 'users',
-      desc: <span>some description</span>,
+      title: '',
+      desc: <span></span>,
       list: [
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
-        '/img/users_alibaba.png',
+       
       ],
     },
   },
